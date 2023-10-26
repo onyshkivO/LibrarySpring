eb175b1c53c3c928962da88ed4194b2f17856f37
Ostapko58 <prostoostap58@gmail.com>
2023-10-26 20:16:01 +0300

    add base test for publication and change return type in service layer from Optional<Entity> to Entity

--- src/test/java/com/onyshkiv/libraryspring/service/PublicationServiceTest.java
+++ src/test/java/com/onyshkiv/libraryspring/service/PublicationServiceTest.java
@@ -1,5 +1,6 @@
 package com.onyshkiv.libraryspring.service;
 
+import com.onyshkiv.libraryspring.dto.DataPageDto;
 import com.onyshkiv.libraryspring.entity.Publication;
 import com.onyshkiv.libraryspring.exception.publication.PublicationNotFoundException;
 import com.onyshkiv.libraryspring.repository.PublicationRepository;
@@ -10,11 +11,16 @@ import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
 
+import java.util.List;
 import java.util.Optional;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyInt;
 import static org.mockito.Mockito.when;
 
@@ -29,7 +35,7 @@ public class PublicationServiceTest {
     private PublicationService publicationService;
 
     @Test
-    public void getPublicationByIdWhenExistTest()  {
+    public void getPublicationByIdWhenExistTest() {
         when(publicationRepository.findById(anyInt()))
                 .thenReturn(Optional.of(new Publication(1, "Publication")));
 
@@ -37,13 +43,34 @@ public class PublicationServiceTest {
         assertThat(publication.getId()).isEqualTo(1);
         assertThat(publication.getName()).isEqualTo("Publication");
     }
+
     @Test
-    public void getPublicationByIdWhenNotExistTest()  {
+    public void getPublicationByIdWhenNotExistTest() {
         when(publicationRepository.findById(anyInt()))
                 .thenReturn(Optional.empty());
 
         assertThatThrownBy(() -> publicationService.getPublicationById(anyInt()))
                 .isInstanceOf(PublicationNotFoundException.class);
+    }
+
+    @Test
+    public void getPublicationsWhenTest() {
+        when(publicationRepository.findAll((Pageable) any()))
+                .thenReturn(new PageImpl<Publication>(List.of(
+                        new Publication(1, "Publication1"),
+                        new Publication(2, "Publication2"),
+                        new Publication(3, "Publication3"))));
+
+        DataPageDto<Publication> pageDto = publicationService.getAllPublications(any());
+
+        List<Publication> publications = pageDto.getData();
+        assertThat(publications.size()).isEqualTo(3);
+        assertThat(publications.get(0).getId()).isEqualTo(1);
+        assertThat(publications.get(0).getName()).isEqualTo("Publication1");
+        assertThat(publications.get(1).getId()).isEqualTo(2);
+        assertThat(publications.get(1).getName()).isEqualTo("Publication2");
+        assertThat(publications.get(2).getId()).isEqualTo(3);
+        assertThat(publications.get(2).getName()).isEqualTo("Publication3");
     }
 
 
