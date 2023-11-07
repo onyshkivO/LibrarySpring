ccccbb0e2f370e054ed677bf50d1a4510379336b
Ostapko58 <prostoostap58@gmail.com>
2023-11-07 22:00:59 +0200

    add docker config files and change data.sql file

---
+++ src/test/java/com/onyshkiv/libraryspring/LibrarySpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.onyshkiv.libraryspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibrarySpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
