5681fa64d44b3be4596c07ea358ce35a122e25ae
Ostapko58 <prostoostap58@gmail.com>
2023-11-10 17:37:51 +0200

    test without library springtests

--- src/test/java/com/onyshkiv/libraryspring/LibrarySpringApplicationTests.java
+++ src/test/java/com/onyshkiv/libraryspring/LibrarySpringApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class LibrarySpringApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+//    @Test
+//    void contextLoads() {
+//    }
 
 }
